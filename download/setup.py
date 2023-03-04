2de2e2e8949220c04f7b70d576c2f32c84a23bb8
Dyrutter <126340978+Dyrutter@users.noreply.github.com>
2023-03-04 08:58:58 -0600

    Update setup.py
    Adjusted to meet flake8 standards

--- download/setup.py
+++ download/setup.py
@@ -4,9 +4,8 @@ from setuptools import setup
 setup(
     name="wandb-utils",
     version=0.1,
-    description="Utilities for interacting with Weights and Biases and mlflow",
-    zip_safe=False,  # avoid eggs, which make the handling of package data cumbersome
-    packages=["wandb_utils"],
+    description="Utilities for interacting with W&B and mlflow",
+    zip_safe=False,  # avoid eggs for handling of package data
     classifiers=[
         "Programming Language :: Python :: 3",
         "Development Status :: 4 - Beta",
