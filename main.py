13dbff679e3f4dade6ecb2b3607c788fb22f3281
Dyrutter <126340978+Dyrutter@users.noreply.github.com>
2023-03-06 09:25:48 -0600

    Update main.py
    Added save_locally

--- main.py
+++ main.py
@@ -12,6 +12,9 @@ def go(config: DictConfig):
     """
     Run MLflow project. From main directory, download step can be run using:
     mlflow run . -P hydra_options="main.execute_steps='download'"
+    Can be run in github using:
+    mlflow run https://github.com/DyRutter/rental_prices.git -v 1.0.1 -P
+    hydra_options="data.sample='sample2.csv'"
     """
     # Setup the wandb experiment. All runs will be grouped under this name
     os.environ["WANDB_PROJECT"] = config["main"]["project_name"]
@@ -33,8 +36,11 @@ def go(config: DictConfig):
             parameters={
                     # URL to raw data sample
                     "file_url": config["data"]["file_url"],
+                    # "raw_data.csv",
                     "artifact_name": config["data"]["raw_data_name"],
+                    # "raw_data"
                     "artifact_type": config["data"]["raw_data_type"],
+                    # "Raw file as downloaded"
                     "artifact_description": config["data"]
                     ["raw_data_description"]})
 
@@ -43,6 +49,7 @@ def go(config: DictConfig):
             os.path.join(root_path, "basic_cleaning"),
             entry_point="main",
             parameters={
+                # "raw_data.csv:latest",
                 "input_artifact": config["data"]["raw_data_artifact"],
                 "output_name": config["data"]["preprocessed_data"],
                 "output_type": "preprocessed_data",
@@ -56,23 +63,25 @@ def go(config: DictConfig):
             entry_point="main",
             parameters={
                 "reference_artifact": config["data"]["reference_dataset"],
+                # "preprocessed_data.csv:latest",
                 "sample_artifact": config["data"]["preprocessed_data_latest"],
                 "kl_threshold": config["data"]["kl_threshold"],
                 "min_price": config["data"]["min_price"],
                 "max_price": config["data"]["max_price"]})
 
-    if "EDA" in steps_to_execute:
-        _ = mlflow.run(
-            os.path.join(root_path, "EDA"),
-            entry_point="main")
+    # if "EDA" in steps_to_execute:
+    #   _ = mlflow.run(
+    #      os.path.join(root_path, "EDA"),
+    #      entry_point="main")
 
     if "segregate" in steps_to_execute:
         _ = mlflow.run(
             os.path.join(root_path, "segregate"),
             entry_point="main",
             parameters={
-                "input_artifact":
-                config["data"]["preprocessed_data_latest"],
+                # preprocessed_data.csv:latest",
+                "input_artifact": config["data"]["preprocessed_data_latest"],
+                "artifact_root": "data",
                 "artifact_type": "segregated_data",
                 "test_size": config["data"]["test_size"],
                 "stratify": config["data"]["stratify"]})
@@ -101,7 +110,9 @@ def go(config: DictConfig):
                 "val_size": config["data"]["test_size"],
                 "stratify_by": config["data"]["stratify_by"],
                 "max_tfidf_features":\
-                config["random_forest_pipeline"]["max_tfidf_features"]})
+                config["random_forest_pipeline"]["max_tfidf_features"],
+                "save_locally":\
+                config["random_forest_pipeline"]["save_locally"]})
 
     if "test_regression_model" in steps_to_execute:
         _ = mlflow.run(
