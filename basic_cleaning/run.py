fef0fb565db29d118713117c7778170684c68ae3
Dyrutter <126340978+Dyrutter@users.noreply.github.com>
2023-03-31 16:20:27 -0500

    Update run.py

--- basic_cleaning/run.py
+++ basic_cleaning/run.py
@@ -3,34 +3,11 @@ import logging
 import os
 from distutils.util import strtobool
 import pandas as pd
-import numpy as np
 import wandb
-from sklearn.impute import SimpleImputer
 
 
 logging.basicConfig(level=logging.INFO, format="%(asctime)-15s %(message)s")
 logger = logging.getLogger()
-
-
-def engineer_dates(data_frame):
-    """
-    Convert date column to feature that represents the number of days passed
-    since the last review.
-    Impute missing date values from an old date, because there hasn't been a
-    review for a long time
-    """
-    # Instantiate imputer & reshape date column for compatability
-    date_imputer = SimpleImputer(strategy='constant', fill_value='2010-01-01')
-    date_column = np.array(data_frame['last_review']).reshape(-1, 1)
-
-    # Impute dates and convert to datetime
-    dates_imputed = date_imputer.fit_transform(date_column)
-    dates_df = pd.DataFrame(dates_imputed).apply(pd.to_datetime)
-
-    # Convert datetime days to ints & get the distance between them
-    dates_df = dates_df.apply(lambda d: (d.max() - d).dt.days, axis=0)
-    data_frame['last_review'] = dates_df
-    return data_frame
 
 
 def drop_features(df):
@@ -93,10 +70,6 @@ def go(args):
     artifact = run.use_artifact(args.input_artifact, type='raw_data')
     artifact_path = artifact.file()
     df = pd.read_csv(artifact_path, low_memory=False)
-
-    # Change dates column
-    logger.info("Reworking dates")
-    df = engineer_dates(df)
 
     # Drop unused features, duplicates, and outliers
     logger.info("Dropping unused features")
